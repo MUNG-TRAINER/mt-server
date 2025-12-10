85538d84f5cdede223f7e8a1d26f1ccce704028b
kihyun2 <jsh99875@gmail.com>
2025-12-10 15:14:23 +0900

    [feat] 회원가입 - 회원 회원가입 - 훈련사 회원가입 - common exception handler - security 일단 다 열어둠

---
+++ src/main/java/com/mungtrainer/mtserver/auth/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.mungtrainer.mtserver.auth.controller;
+
+import com.mungtrainer.mtserver.auth.dto.request.AuthTrainerJoinRequest;
+import com.mungtrainer.mtserver.auth.dto.request.AuthUserJoinRequest;
+import com.mungtrainer.mtserver.auth.dto.response.AuthJoinResponse;
+import com.mungtrainer.mtserver.auth.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+  private final AuthService authService;
+
+  @PostMapping("/join/trainer")
+  public ResponseEntity<AuthJoinResponse> join(@Valid @RequestBody AuthTrainerJoinRequest authTrainerJoinRequest) {
+    AuthJoinResponse response = authService.trainerJoin(authTrainerJoinRequest);
+    return ResponseEntity.ok(response);
+  }
+
+  @PostMapping("/join/user")
+  public ResponseEntity<AuthJoinResponse> join(@Valid @RequestBody AuthUserJoinRequest authUserJoinRequest) {
+    AuthJoinResponse response = authService.userJoin(authUserJoinRequest);
+    return ResponseEntity.ok(response);
+  }
+}
