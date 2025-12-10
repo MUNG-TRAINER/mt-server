e480ebc55c2aa2c14f028d9f074942982bec5ec1
choiseohyun55 <qkr7576@naver.com>
2025-12-10 13:41:47 +0900

    trainer profile 기능 완료

--- src/main/java/com/mungtrainer/mtserver/trainer/entity/TrainerProfile.java
+++ src/main/java/com/mungtrainer/mtserver/trainer/entity/TrainerProfile.java
@@ -45,6 +45,11 @@ public class TrainerProfile extends BaseEntity {
     private String tag;
 
     /**
+     * 등록 코드
+     */
+    private String registCode;
+
+    /**
      * 자격증 이미지 URL
      */
     private String certificationImageUrl;
