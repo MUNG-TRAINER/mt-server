4b4454b3c16f74ad7ac1e16f5f37fba495fee58a
Hellicat <jhccop@naver.com>
2025-12-29 19:25:13 +0900

    [Chore] Edit Code

--- src/main/java/com/mungtrainer/mtserver/counseling/dto/response/CounselingDogResponse.java
+++ src/main/java/com/mungtrainer/mtserver/counseling/dto/response/CounselingDogResponse.java
@@ -1,20 +1,22 @@
 package com.mungtrainer.mtserver.counseling.dto.response;
 
+import java.time.LocalDateTime;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class CounselingDogResponse {
-    private Long counselingId;    // 상담 ID
-    private Long dogId;           // 반려견 ID
-    private String dogName;       // 반려견 이름
-    private String ownerName;     // 보호자 이름
-    private String dogImage;      // 반려견 프로필 이미지 URL (Presigned)
-    private String content;       // 상담 내용 (상담 완료 시에만 존재)
-    private LocalDateTime createdAt; // 상담 신청일시
+	private Long counselingId; // 상담 ID
+	private Long dogId; // 반려견 ID
+	private String dogName; // 반려견 이름
+	private String ownerName; // 보호자 이름
+	private String dogImage; // 반려견 프로필 이미지 URL (Presigned)
+	private String content; // 상담 내용 (상담 완료 시에만 존재)
+	private LocalDateTime createdAt; // 상담 신청일시
+	private Long userId;
+	private String fcmToken;
 }
